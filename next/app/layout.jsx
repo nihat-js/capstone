f07f0e7948a1cd07f0c4e93c5a7525d670c07421
nihat <abdullazadenihat@gmail.com>
2025-06-28 13:16:15 +0400

    next15-e kecdik

--- next/app/layout.jsx
+++ next/app/layout.jsx
@@ -1,8 +1,6 @@
 import './globals.css'
-import { Inter } from 'next/font/google'
 import { Toaster } from 'react-hot-toast'
 
-const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
   title: 'Honeypot Management Dashboard',
@@ -12,7 +10,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>
+      <body>
         <div className="min-h-screen bg-gray-50">
           {children}
         </div>
